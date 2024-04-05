5ba65e9be0fed475f7d2aa6c468cf7822ceca093
Dunateo <bruvalentin84@icloud.com>
2024-04-05 17:55:46 -0400

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,6 @@ fi
 
 main() {
     echo "Starting deploy..."
-
     git config --global --add safe.directory /github/workspace
     git config --global url."https://".insteadOf git://
     ## $GITHUB_SERVER_URL is set as a default environment variable in all workflows, default is https://github.com
